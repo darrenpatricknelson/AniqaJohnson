ab03b9f3fb6da31910bb8dd50608a12b34351460
Darren Nelson <Nelson.darrenp10.dnp@gmail.com>
2023-03-15 14:09:54 +0200

    Update to write up component

--- src/components/Details.js
+++ src/components/Details.js
@@ -4,21 +4,21 @@ const Details = () => {
     return <div className='details_container'>
         <div className='details_content'>
             <div id='who_i_am' className="who_i_am">
-                <h3>Who <span className="text_color">I am</span></h3>
-                <div className='space_border'></div>
+                <h3>Who <span className="text-color">I am</span></h3>
+                <div className='space-border'></div>
                 <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Exercitationem repellendus suscipit impedit qui nemo inventore alias eligendi distinctio, delectus aperiam reiciendis, provident praesentium adipisci, commodi mollitia quo laudantium laborum ea.</p>
                 <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Exercitationem repellendus suscipit impedit qui nemo inventore alias eligendi distinctio, delectus aperiam reiciendis, provident praesentium adipisci, commodi mollitia quo laudantium laborum ea.</p>
             </div>
             <div id='what_i_do' className="what_i_do">
-                <h3>What <span className="text_color">I do</span></h3>
-                <div className='space_border'></div>
+                <h3>What <span className="text-color">I do</span></h3>
+                <div className='space-border'></div>
 
                 <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Exercitationem repellendus suscipit impedit qui nemo inventore alias eligendi distinctio, delectus aperiam reiciendis, provident praesentium adipisci, commodi mollitia quo laudantium laborum ea.</p>
                 <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Exercitationem repellendus suscipit impedit qui nemo inventore alias eligendi distinctio, delectus aperiam reiciendis, provident praesentium adipisci, commodi mollitia quo laudantium laborum ea.</p>
             </div>
             <div id='get_in_touch' className="get_in_touch">
-                <h3>Get <span className="text_color">in touch</span></h3>
-                <div className='space_border'></div>
+                <h3>Get <span className="text-color">in touch</span></h3>
+                <div className='space-border'></div>
                 <div className="contact_details">
                     <p><a href='#.'>Phone number</a></p>
                     <p><a href='#.'>email</a></p>
