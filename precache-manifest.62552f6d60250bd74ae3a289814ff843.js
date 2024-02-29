b45fcc7d298fd1f25fe257eeb46d81ecfbaa99c5
Evan Christians <evan.christians@flexperto.com>
2024-02-29 10:50:50 +0200

    Updates

---
+++ precache-manifest.62552f6d60250bd74ae3a289814ff843.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "879b7bd5eb4e384b2e2f",
+    "url": "/AniqaJohnson/static/css/main.e93600a1.chunk.css"
+  },
+  {
+    "revision": "879b7bd5eb4e384b2e2f",
+    "url": "/AniqaJohnson/static/js/main.879b7bd5.chunk.js"
+  },
+  {
+    "revision": "2fabf96551c3ebd85f84",
+    "url": "/AniqaJohnson/static/js/1.2fabf965.chunk.js"
+  },
+  {
+    "revision": "850201c143d5f092c496",
+    "url": "/AniqaJohnson/static/js/runtime~main.850201c1.js"
+  },
+  {
+    "revision": "c7fbae1d1c733f9154a591894aeacced",
+    "url": "/AniqaJohnson/static/media/cover.c7fbae1d.webp"
+  },
+  {
+    "revision": "261165aa19167c20a15b4794981105c5",
+    "url": "/AniqaJohnson/static/media/ocean.261165aa.webp"
+  },
+  {
+    "revision": "93fcfceee742547dcd1fb80b2731eea6",
+    "url": "/AniqaJohnson/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.085625a77516a68339a3b55de8b787bd.js"
+  "/AniqaJohnson/precache-manifest.62552f6d60250bd74ae3a289814ff843.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/AniqaJohnson/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
