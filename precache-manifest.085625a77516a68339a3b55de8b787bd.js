bc0f4779fbafb3e7f3d42ddf2eb4da1fcee5ee8b
Darren Nelson <Nelson.darrenp10.dnp@gmail.com>
2024-03-01 13:49:05 +0200

    Updates

---
+++ precache-manifest.085625a77516a68339a3b55de8b787bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b62754cd0b06ef3c995a",
+    "url": "/static/css/main.57012405.chunk.css"
+  },
+  {
+    "revision": "b62754cd0b06ef3c995a",
+    "url": "/static/js/main.b62754cd.chunk.js"
+  },
+  {
+    "revision": "00978dcf395300eef63f",
+    "url": "/static/js/1.00978dcf.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c7fbae1d1c733f9154a591894aeacced",
+    "url": "/static/media/cover.c7fbae1d.webp"
+  },
+  {
+    "revision": "261165aa19167c20a15b4794981105c5",
+    "url": "/static/media/ocean.261165aa.webp"
+  },
+  {
+    "revision": "98ab56caf7fb8738d70196f9c2fd78de",
+    "url": "/index.html"
+  }
+];
