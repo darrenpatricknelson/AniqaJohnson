f802d01e1a116e0b2294c243d0986bdaa32d89c8
Darren Nelson <Nelson.darrenp10.dnp@gmail.com>
2024-08-12 15:34:57 +0200

    RE: Address

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -17,7 +17,7 @@ const ContactForm = () => {
                 <div className="contact-details">
                     <p><a href='tel:+2176 923 9857'><FontAwesomeIcon icon={faMobileScreenButton} /> 076 923 9857</a></p>
                     <p><a href='mailto:aniqajohnsoncounselling@gmail.com'><FontAwesomeIcon icon={faEnvelope} /> aniqajohnsoncounselling@gmail.com</a></p>
-                    <p><a href='https://www.google.com/maps/place/Paardevlei.biz/@-34.0912667,18.8114265,21z/data=!4m12!1m5!3m4!2zMzTCsDA1JzI4LjIiUyAxOMKwNDgnNDEuNiJF!8m2!3d-34.0911667!4d18.8115556!3m5!1s0x1dcdb5a58af212bd:0x7c97c7565cfa9ded!8m2!3d-34.0911327!4d18.8115515!16s%2Fg%2F11flyfz525?entry=ttu' target='_blank'><FontAwesomeIcon icon={faLocationDot} /> Unit 103, M01, Baker Square <br /> 18 De Beers Avenue, Paardevlei, 7130</a></p>
+                    <p><a href='https://www.google.com/maps/place/Aniqa+Johnson:+Registered+Counsellor/@-34.0911404,18.810274,18z/data=!3m1!4b1!4m6!3m5!1s0x1dcdb588e381d3f1:0xdd2e977c4a627017!8m2!3d-34.0911404!4d18.8115481!16s%2Fg%2F11l75f8bhd?entry=ttu' target='_blank'><FontAwesomeIcon icon={faLocationDot} /> Unit 103, M01, Baker Square <br /> 18 De Beers Avenue, Paardevlei, 7130</a></p>
                 </div>
                 <p>To contact Aniqa Johnson Counselling and/or request a session, complete the form below:</p>
                 <Form />
