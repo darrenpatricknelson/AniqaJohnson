023dcb1be61db40fffa08906bf97468f00c40639
Darren Nelson <Nelson.darrenp10.gmail.com>
2025-06-05 10:02:40 +0200

    RE: Update to the footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,8 @@ import "./css/Footer.css";
 
 const Footer = () => {
   return (
-    <div className="footer_container">
-      <div className="footer_content">
+    <div className="footer-container">
+      <div className="footer-content">
         <h6>© Copyright - Aniqa Johnson Counselling | Darren Nelson Designs</h6>
       </div>
     </div>
